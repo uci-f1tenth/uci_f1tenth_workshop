86358feeed6ddeeaecea1459c4a69a1c392973d7
dssyk883 <soyeonkwon.0@gmail.com>
2024-10-31 10:00:22 -0700

    Deleted unnecessary files in wall following

--- src/wall_follow/wall_follow/wall_follow_node.py
+++ src/wall_follow/wall_follow/wall_follow_node.py
@@ -5,41 +5,27 @@ import numpy as np
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
 
-MIN_ANGLE = -2.3499999046325684
-MAX_ANGLE = 2.3499999046325684
-INCR_ANGLE = 0.004351851996034384
-
 class WallFollow(Node):
     """ 
     Implement Wall Following on the car
     """
     def __init__(self):
         super().__init__('wall_follow_node')
-        self.get_logger().info("Wall Following Node Started")
 
         lidarscan_topic = '/scan'
         drive_topic = '/drive'
 
         # TODO: create subscribers and publishers
-        self.lidar_sub = self.create_subscription(LaserScan,
-                                                  lidarscan_topic,
-                                                  self.scan_callback,
-                                                  10)
-        
-        self.drive_pub = self.create_publisher(AckermannDriveStamped,
-                                               drive_topic,
-                                               10)
-
 
         # TODO: set PID gains
-        self.kp = 1.0
-        self.kd = 0.1
-        self.ki = 0.01
+        # self.kp = 
+        # self.kd = 
+        # self.ki = 
 
         # TODO: store history
-        self.integral = 0.0
-        self.prev_error = 0.0
-        self.error = 0.0
+        # self.integral = 
+        # self.prev_error = 
+        # self.error = 
 
         # TODO: store any necessary values you think you'll need
 
@@ -56,20 +42,9 @@ class WallFollow(Node):
 
         """
 
-        # angle_min & max = +- 135 degree
-        # range = distance from obstacles (max_range = 30; the max dist from the obstacle is 30 meters)
-        # Supppose the angle is given as rad
-        range_index = int((angle - MIN_ANGLE)/INCR_ANGLE)
-        range_at_angle = range_data[range_index]
+        #TODO: implement
+        return 0.0
 
-        if np.isinf(range_at_angle) or np.isnan(range_at_angle):
-            range_at_angle = 10.0  # Arbitrary large distance if no obstacle detected
-
-        return range_at_angle
-        # if 0 <= range_index < len(range_index):
-        #     if not (np.isNaN(range_at_angle) or np.isinf(range_at_angle)):
-        #         return range_at_angle
-            
     def get_error(self, range_data, dist):
         """
         Calculates the error to the wall. Follow the wall to the left (going counter clockwise in the Levine loop). You potentially will need to use get_range()
@@ -81,9 +56,6 @@ class WallFollow(Node):
         Returns:
             error: calculated error
         """
-
-        theta = 50 * (np.pi/180) # theta set to 50 degrees
-
 
         #TODO:implement
         return 0.0
@@ -117,10 +89,6 @@ class WallFollow(Node):
         error = 0.0 # TODO: replace with error calculated by get_error()
         velocity = 0.0 # TODO: calculate desired car velocity based on error
         self.pid_control(error, velocity) # TODO: actuate the car with PID
-
-        # Test
-        test_range = self.get_range(msg, 0)
-        self.get_logger().info(test_range)
 
 
 def main(args=None):

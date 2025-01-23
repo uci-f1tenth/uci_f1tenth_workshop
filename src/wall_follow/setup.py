ace69de75a5ff66ea498147b5ce66771a6af3a36
dssyk883 <soyeonkwon.0@gmail.com>
2025-01-22 17:49:19 -0800

    Fixed configurations

--- src/wall_follow/setup.py
+++ src/wall_follow/setup.py
@@ -5,7 +5,7 @@ package_name = 'wall_follow'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages,
+    packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),

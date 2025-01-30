978063148737ee59b0808b936f94be69e97806bd
yuywe <jxdev5.11@gmail.com>
2025-01-29 18:54:31 -0800

    fix: terminal data spam from lidar scan

--- src/dreamer_node/agents/dreamer_agent.py
+++ src/dreamer_node/agents/dreamer_agent.py
@@ -24,6 +24,7 @@ class Dreamer(Node):
 
         # variables
         self.time = self.get_clock().now()
+        self.scan_num = 0
 
         # subscribers/publishers
         self.sub_scan = self.create_subscription(
@@ -32,11 +33,13 @@ class Dreamer(Node):
             self.scan_callback,  # run function
             10
         )
+        
         self.pub_drive = self.create_publisher(
             AckermannDriveStamped, 
             self.const.DRIVE_TOPIC, 
             10
         )
+        
         self.pub_scan = self.create_publisher(
             LaserScan,
             self.const.LIDAR_TOPIC,
@@ -52,24 +55,37 @@ class Dreamer(Node):
             assert f'Error: last laser scan time is {last_scan_time}. Limit = 10Hz'
             return
         
-        # receive LiDAR scan rte
+        # receive LiDAR scan rate
         self.time = current_stamp
-        print('dreamer received LIDAR scan @ rate:', last_scan_time / 1e9) # TODO: debug for scan rate
-        observations = dict()
-        observations['lidar'] = list(np.flip(np.array(scan_msg.ranges, dtype=float))) # include liDAR observations
-        print("observation = ", observations); # TODO: debug scan_msg.range
-        assert len(observations['lidar']) > 0, "Missing observations. Are you sure everything is linked properly?"
-
+        self.scan_num += 1
+        
+        lidar_data = np.array(scan_msg.ranges, dtype = float)
+        
+        if len(lidar_data) == 0:
+            self.get_logger().info("Invalid Lidar data")
+           
+        min_dist = np.min(lidar_data)
+        max_dist = np.max(lidar_data)
+        mean_dist = np.mean(lidar_data)
+        std_dist = np.std(lidar_data)
+        
+        if self.scan_num % 5 == 0:
+            self.get_logger().info(
+                f"LIDAR Timestamp: {current_stamp.nanoseconds}, Scan Rate: {last_scan_time / 1e9:.6f}s"
+                f"Min: {min_dist:.4f}m, Max : {max_dist:.4f}m, Mean: {mean_dist:.4f}m, Std: {std_dist:.4f}m"
+            )
+        
+        #print('dreamer received LIDAR scan @ rate:', last_scan_time / 1e9) # TODO: debug for scan rate
+        
         # Gausian noise(optional implementation)
-        obs_lidar = observations['lidar'][:1080]
+        observations = {'lidar': lidar_data[:1000].tolist()}
         
         # optional noised
         # extra_noise_stddev = 0.3 # 0.3m
         # extra_noise = np.random.normal(0, extra_noise_stddev, obs_lidar.shape)
         #obs_lidar_noised = obs_lidar + extra_noise
         #obs_lidar_noised = np.clip(obs_lidar_noised, 0, None)
-
-        observations['lidar'] = obs_lidar # obs_lidar_noised for noise
+        
         scan_noised = scan_msg
         scan_noised.ranges = observations['lidar']
 

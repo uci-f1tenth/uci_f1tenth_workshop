fd306e114f061f0dda35f00bfac86a2d90a855bc
Hyunsu Lim <hyunsulim@dhcp-172-31-150-061.mobile.uci.edu>
2025-02-03 15:37:21 -0800

    feat: added environment setup

---
+++ src/dreamer_node/env/racerenv.py
@@ -0,0 +1,22 @@
+import gymnasium as gym
+
+class RacerEnv:
+    metadata = {}
+    def __init__(self):
+        pass
+
+    @property
+    def observation_space(self):
+        pass
+
+    @property
+    def action_space(self):
+        pass
+
+    def step(self):
+        pass
+
+    def reset(self):
+        pass
+
+

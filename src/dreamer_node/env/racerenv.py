ff10ab8b5a9cb2f1d2890671876bacd5445d6081
yuywe <jxdev5.11@gmail.com>
2025-02-08 21:38:49 -0800

    chore: code structure based on Lukas' code

--- src/dreamer_node/env/racerenv.py
+++ src/dreamer_node/env/racerenv.py
@@ -1,4 +1,8 @@
 import gymnasium as gym
+import numpy as np
+
+MIN_LIDAR = 0.021
+MAX_LIDAR = 30.0
 
 class RacerEnv:
     metadata = {}
@@ -7,16 +11,51 @@ class RacerEnv:
 
     @property
     def observation_space(self):
-        pass
+        '''
+        returns a dictionary containing LiDAR data & odomoetry data (optional)
+        '''
+        
+        self.lidar_space = gym.spaces.Box(low = np.array([MIN_LIDAR] * 1081, dtype = np.float32),
+                                          high = np.array([MAX_LIDAR] * 1081, dtype = np.float32),
+                                          shape = (1081,), dtype = np.float32)
+        spaces = {"lidar": self.lidar_space}
+        
+        if self.include_odom:
+            self.odom_space = gym.spaces.Box(low = -np.inf, high = np.inf, shape = (1081,), dtype = np.float32)
+            spaces["odom"] = self.odom_space
+        return gym.spaces.Dict(spaces)
+        
 
     @property
     def action_space(self):
         pass
 
-    def step(self):
-        pass
+    def step(self, action, lidar_data, odom_data = None):
+        #normalized_lidar = normalize_lidar(lidar_data)
+        steering = self.denormalize(action[0], self.min_sterring, self.max_steering)
+        speed = self.denormalize(action[1], self.min_speed, self.max_speed)
+        
+        lidar_data = np.clip(lidar_data, self.lidar_space.low, self.lidar_space.high)
+        
+        observation = {"lidar": lidar_data}
+        if self.include_odom and odom_data != None:
+            observation["odom"] = np.array(odom_data, dtype = np.float32)
 
-    def reset(self):
-        pass
+        info = {}
+        return observation, info
+        
+
+    def reset(self, seed = None, options = None, lidar_data = None, odom_data = None):
+        if lidar_data is None:
+            raise ValueError("Reset: LiDAR data does not exist")
+        
+        lidar_data = np.clip(lidar_data, self.lidar_space.low, self.lidar_space.high)
+        
+        observation = {"lidar": lidar_data}
+        if self.include_odom and odom_data != None:
+            observation["odom"] = np.array(odom_data, dtype = np.float32)
+            
+        info = {}
+        return observation, info
 
 #whatsup

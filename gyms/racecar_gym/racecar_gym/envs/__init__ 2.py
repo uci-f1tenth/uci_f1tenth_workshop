27518e8ca3e32cbbcdaf2b422277ba9b66d90023
Alistair Keiller <alistair@keiller.net>
2025-02-19 17:43:16 -0800

    remove all the 2 files

--- gyms/racecar_gym/racecar_gym/envs/__init__ 2.py
+++
@@ -1,5 +0,0 @@
-from . import gym_api
-from . import pettingzoo_api
-from .scenarios import MultiAgentScenario, SingleAgentScenario
-
-__all__ = ["pettingzoo_api", "gym_api", "MultiAgentScenario", "SingleAgentScenario"]

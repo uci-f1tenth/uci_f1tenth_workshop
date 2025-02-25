b3de77db35934ed00cae42b788871edb6df959e3
Rohan Jayasekara <rohanjay321@gmail.com>
2025-02-24 18:09:20 -0800

    removed cnn key

--- dreamer_node/util/constants.py
+++ dreamer_node/util/constants.py
@@ -68,7 +68,7 @@ class Config:
         # Encoder configuration (updated from f1tenth)
         self.encoder = {
             "mlp_keys": "pose|velocity|acceleration|lidar",  # Process these with MLP
-            "cnn_keys": "image",  # Process this with CNN
+            "cnn_keys": "$^",  # Process this with CNN
             "act": "SiLU",
             "norm": True,
             "cnn_depth": 32,
@@ -82,7 +82,7 @@ class Config:
         # Decoder configuration (updated from f1tenth)
         self.decoder = {
             "mlp_keys": "pose|velocity|acceleration|lidar",  # Decode these with MLP
-            "cnn_keys": "image",  # Decode this with CNN
+            "cnn_keys": "$^",  # Decode this with CNN
             "act": "SiLU",
             "norm": True,
             "cnn_depth": 32,

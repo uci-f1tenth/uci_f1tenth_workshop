ffea5e383b5c2a5c97c900620ede1c4280abc8db
waterkimchi <hyunsul2@uci.edu>
2025-02-28 17:21:53 -0800

    feat: added compile script

---
+++ docs/compile.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+#Run the Script from the folder you are in...
+CURRENT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+compile=""
+biberarg=""
+CMD_LATEX=lualatex
+# avoid $TERM warning
+export TERM=xterm-256color
+
+# Function to remove temporary files from previously crashed runs
+cleanup() {
+	rm -f ./*.bbl ./*.blg ./*.aux ./*.bcf ./*.ilg ./*.lof ./*.log ./*.lot ./*.nlo ./*.nls* ./*.out ./*.toc ./*.run.xml ./*.sub ./*.suc ./*.syc ./*.sym
+}
+
+# Remove existing PDF and temporary files at the beginning
+rm -f "$CURRENT_DIR/Development_Guide.pdf"
+cleanup
+
+echo "Compiling in Language: $1"
+if [ "$1" = "en" ] || [ "$2" = "en" ]; then
+	compile="$CMD_LATEX --shell-escape --jobname=\"development_guide\" \"\def\FOMEN{}\input{$CURRENT_DIR/Development_Guide/dissertation.tex}\""
+	biberarg="$CURRENT_DIR/Development_Guide"
+else
+	compile="$CMD_LATEX --shell-escape \"$CURRENT_DIR/Development_Guide/dissertation.tex\""
+	biberarg="$CURRENT_DIR/Development_Guide"
+fi
+
+eval "$compile"
+RETVAL="$?"
+if [[ "${RETVAL}" -ne 0 ]]; then
+	echo "First $CMD_LATEX run failed"
+	exit ${RETVAL}
+fi
+
+biber "$biberarg"
+RETVAL="$?"
+if [[ "${RETVAL}" -ne 0 ]]; then
+	echo "biber run failed"
+	exit ${RETVAL}
+fi
+
+eval "$compile"
+RETVAL="$?"
+if [[ "${RETVAL}" -ne 0 ]]; then
+	echo "Second $CMD_LATEX run failed"
+	exit ${RETVAL}
+fi
+
+eval "$compile"
+RETVAL="$?"
+if [[ "${RETVAL}" -ne 0 ]]; then
+	echo "Third $CMD_LATEX run failed"
+	exit ${RETVAL}
+fi
+
+# Remove temporary files at the end
+cleanup
+
+echo "PDF Compile: Success"
+
+exit 0

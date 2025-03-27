4b4f3c6170fc40956590736be6ba260507d28156
Jeffery Lee <106774296+yuywe@users.noreply.github.com>
2025-03-26 21:15:17 -0700

    Update dreamer_node/dreamer/dream.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- dreamer_node/dreamer/dream.py
+++ dreamer_node/dreamer/dream.py
@@ -275,7 +275,7 @@ def main(config):
     while agent._step < config.steps + config.eval_every:
         # Logging
         progress_percent = (agent._step / config.steps) * 100
-        print(f"Training progress: {progress_precent:.2f}%")
+        print(f"Training progress: {progress_percent:.2f}%")
         logger.scalar("training_progress", progress_precent)
         logger.write(step=agent._step)
         
